ded50dbbcfe4fcf9c1cdf58359d978b98fe9940e
Katя Zubko <plutofffka@gmail.com>
2015-11-12 19:43:04 +0300

    Two last ifs w/ arrays

--- js/check.js
+++ js/check.js
@@ -4,22 +4,30 @@
 
 function getMessage(a, b){
   if (typeof a === 'boolean') {
-    if (a == true)
+    if (a === true)
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     else
       return 'Переданное GIF-изображение не анимировано';
   }
-  if (typeof a == 'number')
+  if (typeof a === 'number')
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов';
 
-  if (a instanceof 'Array') {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
+    for (var j = 0; j < a.length; j++) {
+      (a[j]) *= (b[j]);
+      square += (a[j]);
+    }
+    return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+  }
+
+  if (Array.isArray(a)) {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
-      sum += a[i];
+      sum += (a[i]);
     }
     return 'Количество красных точек во всех строчках изображения: ' + sum;
   }
-
 }
 
 
