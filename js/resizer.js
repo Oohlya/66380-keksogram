cde615d94758e05d3b10370eedca572f8f8d432e
Katя Zubko <plutofffka@gmail.com>
2015-11-29 12:46:52 +0300

    Improved overlay and picture drawing

--- js/resizer.js
+++ js/resizer.js
@@ -106,12 +106,10 @@
 
       var displX = -(this._resizeConstraint.x + this._resizeConstraint.side / 2);
       var displY = -(this._resizeConstraint.y + this._resizeConstraint.side / 2);
-      // Отрисовка изображения на холсте. Параметры задают изображение, которое
-      // нужно отрисовать и координаты его верхнего левого угла.
-      // Координаты задаются от центра холста.
 
-      // Oohlya's code see below
-      this._ctx.drawImage(this._image, displX, displY);
+      // Overlay
+      this._ctx.globalCompositeOperation = 'destination-over';
+
       this._ctx.fillStyle = 'rgba(0,0,0,0.8)';
       this._ctx.fillRect(displX, displY, this._container.width, this._container.height);
       this._ctx.clearRect(
@@ -119,11 +117,13 @@
         (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
         this._resizeConstraint.side - this._ctx.lineWidth / 2,
         this._resizeConstraint.side - this._ctx.lineWidth / 2);
-      this._ctx.drawImage(this._image, this._resizeConstraint.x, this._resizeConstraint.y, this._resizeConstraint.side, this._resizeConstraint.side,
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
-        this._resizeConstraint.side - this._ctx.lineWidth / 2,
-        this._resizeConstraint.side - this._ctx.lineWidth / 2);
+
+      // Отрисовка изображения на холсте. Параметры задают изображение, которое
+      // нужно отрисовать и координаты его верхнего левого угла.
+      // Координаты задаются от центра холста.
+      this._ctx.drawImage(this._image, displX, displY);
+
+      this._ctx.globalCompositeOperation = 'source-over';
 
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
